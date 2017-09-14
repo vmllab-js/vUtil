d24f016cd0166bfd2b66102f2033febdd4052edc
Rhine Liu <rhine@rhine.me>
2017-09-14 17:54:49 +0800

    update Cookie

--- src/vUtil.Cookie.js
+++ src/vUtil.Cookie.js
@@ -2,4 +2,30 @@
  * @author: Rhine Liu
  */
 
-vUtil.Cookie = {};
+vUtil.Cookie = {
+	/**
+	 * set the value of a key in cookie
+	 * @param key {String}
+	 * @param value {String}
+	 * @param expires {Number} seconds
+	 */
+	set (key, value, expires) {
+		document.cookie = key + "=" + escape(value) + (expires != undefined ? ";expires=" + new Date(Date.now() + expires * 1000).toGMTString() : "");
+	},
+	/**
+	 * get the value of a key in cookie
+	 * @param key {String}
+	 * @returns {String|Null}
+	 */
+	get (key) {
+		let a = document.cookie.match(new RegExp("(^| )" + key + "=([^;]*)(;|$)"));
+		return a ? unescape(a[2]) : null;
+	},
+	/**
+	 * remove a key from cookie
+	 * @param key {String}
+	 */
+	remove (key) {
+		this.set(key, '', 0);
+	}
+};
