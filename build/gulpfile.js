971d1eb7d9b69f6f727ab67939abeb11118f3722
Rhine Liu <rhine@rhine.me>
2017-09-14 16:46:04 +0800

    support ES6 with babel

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var babel = require('gulp-babel');
 var concat = require('./build.js');
 
 gulp.task('build', [], function () {
 	return gulp.src('../src/*.js')
+		.pipe(babel({presets: ['env']}))
 		.pipe(concat('vUtil.js', '"__vUtil__"'))
 		.pipe(gulp.dest('../dist/'))
 		.pipe(uglify({preserveComments: 'license'}))
