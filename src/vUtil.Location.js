f354d193e0eb5d6c298661cf4c45ec175e5f44ec
Rhine Liu <rhine@rhine.me>
2017-09-14 18:16:23 +0800

    update Location

--- src/vUtil.Location.js
+++ src/vUtil.Location.js
@@ -2,4 +2,13 @@
  * @author: Rhine Liu
  */
 
-vUtil.Location = {};
+vUtil.Location = {
+	search (key) {
+		var r = location.search.match(new RegExp("(^|&)" + key + "=([^&]*)(&|$)"));
+		return r ? decodeURIComponent(r[2]) : null;
+	},
+	hash (key) {
+		var r = location.hash.match(new RegExp("([\?|&])" + key + "=([^&]*)(&|$)"));
+		return r ? decodeURIComponent(r[2]) : null;
+	}
+};
